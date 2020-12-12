532432efb60d1f25b520be65e0aef6d4439fbcf3
tkm0424 <fcp.t4ku@gmail.com>
2020-12-12 23:44:41 +0900

    cli

--- spec/requests/tests_spec.rb
+++ spec/requests/tests_spec.rb
@@ -1,10 +1,2 @@
 require 'rails_helper'
 
-RSpec.describe "Tests", type: :request do
-  describe "GET /" do
-    it "Bye, world!が表示されること" do
-      get root_path
-      expect(response.body).to include "Bye, world!"
-    end
-  end
-end
