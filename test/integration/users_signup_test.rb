b522b69e3ac59537380041605079fa4e681e845d
tkm0424 <fcp.t4ku@gmail.com>
2021-01-03 01:25:31 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'  
+    assert_not flash.empty?    
+  end
+end
